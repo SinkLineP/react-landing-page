5c24f5967cb511434ed5d349cd7b09f46eeb2fee
SinkLineP <popovartem505@gmail.com>
2020-10-04 15:27:06 +0300

    update SectorList to Mobile

--- src/components/SectorsList/SectorsList.js
+++ src/components/SectorsList/SectorsList.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import './SectorsList.scss'
-import SectorItem from "./SectorItem";
 
-const SectorsList = ({title, subtitle, items}) => {
-	const itemsList = items.map((sector, i) => <SectorItem sector={sector} number={i+1} key={i}/>)
-	
-	return (
-		<section className="sectors container">
-			<h2 className="sectors-title">{title}</h2>
-			<p className="sectors-subtitle">{subtitle}</p>
-			<div className="sectors-list">
-				{itemsList}
-			</div>
-		</section>
-	);
+import SectorItem from "./SectorItem";
+import "./SectorsList.scss"
+
+const Sectors = ({ title, subtitle, items }) => {
+    const itemsList = items.map((sector, i) => <SectorItem sector={sector} number={i + 1} key={i} />);
+
+    return (
+        <section className="sectors" id="sectors">
+            <div className="container">
+                <h2 className="sectors-title" id="politics">{title}</h2>
+                <p className="sectors-subtitle">{subtitle}</p>
+                <div className="sectors-list">
+                    {itemsList}
+                </div>
+            </div>
+        </section>
+    );
 };
 
-export default SectorsList;
+export default Sectors;
